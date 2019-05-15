84513eeea3b00c7d8c339c82d8dcda1e3aa0c07a
Anffore <alex.poul.v@yandex.ru>
2019-05-15 21:28:42 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using WPFTourApp.Model;
+
+namespace WPFTourApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TourDatabase db = new TourDatabase();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Click(object sender, RoutedEventArgs e)
+        {
+            var tours = db.Tours.Where(t => t.From == fromTxt.Text)
+                                .Where(t => t.Where == whereTxt.Text)
+                                .Where(t => t.TripDate == calTxt.SelectedDate);
+
+            if(tours.Any()) //Проверка на найденные туры
+            {              
+                this.Hide();
+                ToursWindow tourWindow = new ToursWindow();
+                tourWindow.dataGridTour.ItemsSource = tours.ToList<Tour>();
+                tourWindow.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Заданных туров нет!");
+            }
+        }
+
+        //Завершить работу приложения
+        private void ImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ ToursWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFTourApp
+{
+    /// <summary>
+    /// Логика взаимодействия для ToursWindow.xaml
+    /// </summary>
+    public partial class ToursWindow : Window
+    {
+        public ToursWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void HomeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+
+        }
+    }
+}
